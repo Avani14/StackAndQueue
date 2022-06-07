7bfef3d9b5bd908578972d4ebe39129e84d1e095
Avani14 <trivediavani14@gmail.com>
2022-06-07 16:25:34 +0530

    Enqueue

---
+++ src/StackUsing_LinkedList/Queue.java
@@ -0,0 +1,31 @@
+package StackUsing_LinkedList;
+
+public class Queue extends Common implements IQueue{
+    @Override
+    public void enqueue(int data) {
+        Node node = new Node(data);
+        if(head == null)
+        {
+            head = node;
+            return;
+        }
+        Node temp = head;
+        while(temp.next != null)
+        {
+            temp = temp.next;
+        }
+        temp.next = node;
+    }
+
+    @Override
+    public int dequeue() {
+        if(head == null)
+        {
+            System.out.println("The queue is empty");
+            return 0;
+        }
+        int data = head.data;
+        head = head.next;
+        return data;
+    }
+}
